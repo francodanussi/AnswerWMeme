98196b41af44c43144d492a0848458c864961102
francodanussi <franco_danussi@hotmail.com>
2016-12-08 14:39:45 -0300

    Primer Commit. Hello Meme.

---
+++ app/src/main/java/app/com/magentastudio/answerwmeme/MainActivityMeme.java
@@ -0,0 +1,13 @@
+package app.com.magentastudio.answerwmeme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityMeme extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_meme);
+    }
+}
